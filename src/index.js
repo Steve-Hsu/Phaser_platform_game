aef7107f94033869acce0714f3cb8165dcf98c73
Steve-Hsu <wuujdsc@gmail.com>
2023-01-10 12:31:10 +0700

    1st commit, basic settings

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+import Phaser from "phaser";
+import PlayScene from './scenes/Play'
+
+const WIDTH = 1280;
+const HEIGHT = 600;
+
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+}
+
+const Scenes = [PlayScene];
+const createScene = Scene => new Scene(SHARED_CONFIG);
+const initScenes = () => Scenes.map(createScene);
+
+const config = {
+  type: Phaser.AUTO,
+  ...SHARED_CONFIG,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      // debug: true,
+    }
+  },
+  scene: initScenes()
+};
+
+new Phaser.Game(config);
