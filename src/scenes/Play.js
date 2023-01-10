cb86558f96d9afb9fd37e3a4d60bd39c7431ddee
Steve-Hsu <wuujdsc@gmail.com>
2023-01-10 17:48:20 +0700

    66.Adds Collidable layer

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,21 +6,37 @@ class Play extends Phaser.Scene {
   }
   create() {
     const map = this.createMap()
-    this.createLayers(map)
+    const layers = this.createLayers(map)
+    const player = this.createPlayer();
 
+    this.physics.add.collider(player, layers.platformsColliers)
   }
 
   createMap() {
     const map = this.make.tilemap({ key: 'map' });
-    const layers = map.addTilesetImage('main_lev_build_1', 'tiles-1');
+    map.addTilesetImage('main_lev_build_1', 'tiles-1');
     return map
   }
 
   createLayers(map) {
     const titleset1 = map.getTileset('main_lev_build_1')
+    const platformsColliers = map.createStaticLayer('platforms_colliders', titleset1);
     const environment = map.createStaticLayer('environment', titleset1);
     const platforms = map.createStaticLayer('platforms', titleset1);
-    return { environment, platforms }
+
+
+    // Make collision
+    // platformsColliers.setCollisionByExclusion(-1, true); // standard code
+    platformsColliers.setCollisionByProperty({ collider: true }); // Only when we set the tile with custom property
+
+    return { environment, platforms, platformsColliers }
+  }
+
+  createPlayer() {
+    const player = this.physics.add.sprite(100, 250, 'player');
+    player.body.setGravityY(500); // set so the player will fill in Y direction
+    player.setCollideWorldBounds(true);
+    return player
   }
 }
 
